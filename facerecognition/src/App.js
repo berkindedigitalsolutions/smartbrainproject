b14596b0f6b0dbdb7197bba69980cdb948897006
Berkin Cetin <berkincetin@Berkins-MacBook-Pro.local>
2020-06-07 11:45:32 -0400

    .

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -6,6 +6,7 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
 
 import './App.css';
 
@@ -34,23 +35,44 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl:'',
+      box: {},
+      route: 'signin'
     }
+  }
+
+  calculateFaceLocation = (data) => {
+      const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+      const image = document.getElementById('inputimage');
+      const width = Number(image.width);
+      const height = Number(image.height);
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row*height,
+        rightCol: width - (clarifaiFace.right_col*width),
+        bottomRow: height - (clarifaiFace.bottom_row*height)
+      }
+    }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({ box })
   }
   onInputChange = (event) => {
     this.setState({input: event.target.value})
     }
   onButtonSubmit = () => {
       this.setState({imageUrl: this.state.input})
-      app.models.predict(
-      Clarifai.COLOR_MODEL,this.state.input)
-      .then(
-      function(response) {
-        console.log("Response is",response);
-            },
-      function(err) {
-        // there was an error
-      }
-    );
+      app.models
+      .predict(
+        "a403429f2ddf4b49b307e318f00e528b",
+        this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+  
+  }
+
+  onRouteChange = (route) => {
+    this.setState({route: route});
   }
 
   render() {
@@ -59,7 +81,12 @@ class App extends Component {
         <Particles className='particles'
           params ={particlesOptions}
         />
-        <Navigation />
+        <Navigation onRouteChange={this.onRouteChange} />  
+        { this.state.route ==='signin' ? 
+        <Signin onRouteChange={this.onRouteChange} />
+        :
+        <div>
+
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} 
@@ -67,7 +94,9 @@ class App extends Component {
         />
 
         
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+        </div>
+        }    
       </div>
     );
   }
