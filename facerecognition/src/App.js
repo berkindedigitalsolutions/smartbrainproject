8ceaaf10d90e8e2daa9e03a463235c49faa06d35
Berkin Cetin <berkincetin@Berkins-MacBook-Pro.local>
2020-06-07 08:48:35 -0400

    .

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component} from 'react';
 import Particles from 'react-particles-js';
 
 import Navigation from './components/Navigation/Navigation';
@@ -18,21 +18,40 @@ const particlesOptions = {
     }
   }
 }
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-        params ={particlesOptions}
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*
-      
-      <FaceRecognition />*/}
-    </div>
-  );
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params ={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit}
+        />
+        {/*
+        
+        <FaceRecognition />*/}
+      </div>
+    );
+  }
+
 }
 
 export default App;
