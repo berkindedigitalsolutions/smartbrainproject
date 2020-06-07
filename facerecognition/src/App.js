f508d72b7ae122feda13d589b11d50768f091a54
Berkin Cetin <berkincetin@Berkins-MacBook-Pro.local>
2020-06-07 09:45:48 -0400

    .

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -5,7 +5,15 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+
 import './App.css';
+
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '223670dbbd6d48e19b8da62249c07922'
+});
 
 const particlesOptions = {
   particles: {
@@ -25,14 +33,26 @@ class App extends Component {
     super();
     this.state = {
       input: '',
+      imageUrl:'',
     }
   }
   onInputChange = (event) => {
-    console.log(event.target.value);
-  }
+    this.setState({input: event.target.value})
+    }
   onButtonSubmit = () => {
-    console.log('click');
+      this.setState({imageUrl: this.state.input})
+      app.models.predict(
+      Clarifai.COLOR_MODEL,this.state.input)
+      .then(
+      function(response) {
+        console.log("Response is",response);
+            },
+      function(err) {
+        // there was an error
+      }
+    );
   }
+
   render() {
     return (
       <div className="App">
@@ -45,9 +65,9 @@ class App extends Component {
         <ImageLinkForm onInputChange={this.onInputChange} 
         onButtonSubmit={this.onButtonSubmit}
         />
-        {/*
+
         
-        <FaceRecognition />*/}
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
