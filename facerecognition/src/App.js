8a57975b6172b9b09268e2fcb32c9ee5cde8c7a3
Berkin Cetin <berkincetin@Berkins-MacBook-Pro.local>
2020-06-08 14:11:49 -0400

    .

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -29,25 +29,26 @@ const particlesOptions = {
   }
 }
 
+const initialState = {
+  input: '',
+  imageUrl:'',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+    
+  }
+}
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl:'',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-        
-      }
-    }
+    this.state = initialState;
   }
 
   // componentDidMount() {
@@ -103,6 +104,7 @@ class App extends Component {
           .then(count => {
             this.setState(Object.assign(this.state.user, {entries: count}))
           })
+          .catch(console.log);
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
@@ -112,7 +114,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route ==='signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
